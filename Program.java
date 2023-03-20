43777b3fca58405eae990b575147e2f4704129fb
yana sarvarova <serg.yana@mail.ru>
2023-03-20 18:33:32 +0300

    Add Homework

---
+++ Program.java
@@ -0,0 +1,17 @@
+public class Program {
+    public static void main(String[] args) {
+        RedBlackTree redBlackTree = new RedBlackTree();
+
+        redBlackTree.add(15);
+        redBlackTree.add(5);
+        redBlackTree.add(1);
+        redBlackTree.add(7);
+        redBlackTree.add(9);
+        redBlackTree.add(12);
+        redBlackTree.add(8);
+        redBlackTree.add(16);
+        redBlackTree.add(20);
+        redBlackTree.add(21);
+    }
+
+}
